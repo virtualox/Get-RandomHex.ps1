ad34e3f5a03f9e5afb67a3ebdf6c1307dfdb1fbd
VirtualOx <37578273+virtualox@users.noreply.github.com>
2019-07-18 15:36:53 +0200

    Create Get-RandomHex.ps1

---
+++ Get-RandomHex.ps1
@@ -0,0 +1,10 @@
+function Get-RandomHex {
+    param(
+        [int] $Bits = 256
+    )
+    $bytes = new-object 'System.Byte[]' ($Bits/8)
+    (new-object System.Security.Cryptography.RNGCryptoServiceProvider).GetBytes($bytes)
+    (new-object System.Runtime.Remoting.Metadata.W3cXsd2001.SoapHexBinary @(,$bytes)).ToString()
+}
+
+Get-RandomHex -Bits 24
