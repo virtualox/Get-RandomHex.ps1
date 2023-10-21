273c28eb1d89fb6861fa64edd3e292a93e81968e
VirtualOx <37578273+virtualox@users.noreply.github.com>
2023-10-21 08:30:38 +0200

    Update Get-RandomHex.ps1
    Code cleaning and added synopsis

--- Get-RandomHex.ps1
+++ Get-RandomHex.ps1
@@ -1,14 +1,40 @@
+<#
+.SYNOPSIS
+Generates a random hexadecimal string using cryptographically secure random number generation.
+
+.DESCRIPTION
+The Get-RandomHexString function returns a random hexadecimal string of a specified bit length. The default bit length is 256.
+
+.PARAMETER BitLength
+The length (in bits) of the hexadecimal string to be generated. Default is 256 bits.
+
+.EXAMPLE
+Get-RandomHexString -BitLength 128
+Generates a 128-bit long hexadecimal string.
+
+.NOTES
+The function uses the System.Security.Cryptography.RNGCryptoServiceProvider class for cryptographically secure random number generation.
+#>
+
+[CmdletBinding()]
+param(
+    [int] $BitLength = 256
+)
+
 function Get-RandomHexString {
-    param(
-        [int] $BitLength = 256
-    )
-    $byteLength = $BitLength / 8
-    $bytes = New-Object 'System.Byte[]' $byteLength
-    $rngCryptoProvider = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
-    $rngCryptoProvider.GetBytes($bytes)
-    $hexBinary = New-Object System.Runtime.Remoting.Metadata.W3cXsd2001.SoapHexBinary -ArgumentList @(,$bytes)
     
-    return $hexBinary.ToString()
+    $byteLength = $BitLength / 8
+    $bytes = [byte[]]::new($byteLength)
+
+    $rngCryptoProvider = [System.Security.Cryptography.RNGCryptoServiceProvider]::new()
+    $rngCryptoProvider.GetBytes($bytes)
+
+    # Properly dispose the crypto service provider
+    $rngCryptoProvider.Dispose()
+
+    # Convert bytes to hex string
+    $hexString = [BitConverter]::ToString($bytes) -replace '-'
+    return $hexString
 }
 
-Get-RandomHexString -BitLength 128
+Get-RandomHexString -BitLength $BitLength
